6b2af93344f19a4e75cc67c5ae797e8c22ef2599
Gerardo Ortega <geraldavid7@gmail.com>
2021-09-03 14:20:03 -0600

    Generating Gem

---
+++ lib/price_calculator.rb
@@ -0,0 +1,6 @@
+require "price_calculator/version"
+
+module PriceCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/price_calculator/version.rb
@@ -0,0 +1,3 @@
+module PriceCalculator
+  VERSION = "0.1.0"
+end
